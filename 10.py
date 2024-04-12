83a94e136e8350af8241822055ade4df65259f42
Admin <maayush235@gmail.com>
2024-04-12 08:26:02 +0530

    radhey radhey

--- 10.py
+++ 10.py
@@ -5,21 +5,18 @@ num = int(input("Enter the value : "))
 if num > 1 :
 
     for i in range(2 , (num // 2) + 1) :
+        if num % i == 0:
+            print("This is not a prime number")
+            break
+    else :
+        print("This is a prime number")
         
-        if num % i :
-            print("This is prime number")
-            break
-        else :
-            print("This is a not prime")
-            break
 else : 
      print("Enter more than 1 number")
 
 
 
-    
-  
-  
+
   
   
   # if num % 2 != 0 & num % 3 != 0 & num % 5 != 0 & num % 7 != 0 :
