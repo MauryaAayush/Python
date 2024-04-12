c2d7ec0fe5f39a598aa6aeb628ad9918229471d7
Admin <maayush235@gmail.com>
2024-04-12 08:43:52 +0530

    Radhey  Radhey

--- 9.py
+++ 9.py
@@ -1,2 +1,19 @@
 # write a program to generate fibonacii series
+len = int(input("Enter the length of series = "))
 
+start = 0
+last = 1
+next_number = last
+count = 1
+
+print("0  1 ", end= " ")
+
+while count <= len :
+        print(next_number, end = "  ")
+        count += 1
+        
+        start = last
+        last = next_number
+        
+        next_number = start + last
+# print(" achha")
