7e20cbf35d320d948d689a3d973053f2c94385d4
Admin <maayush235@gmail.com>
2024-04-12 08:52:46 +0530

    Radhey Radhey

--- 11.py
+++ 11.py
@@ -1,1 +1,8 @@
 # write a program to concatenate two strings.
+
+Fname = str(input("Enter your first name : "))
+Lnamae = str(input("Enter your last name : "))
+
+Full_name = Fname + Lnamae
+
+print(Full_name)
--- 12.py
+++ 12.py
@@ -1,1 +1,4 @@
-# create a program of to reverse a string
+# # create a program of to reverse a string
+# def reverse(string) :
+
+# # print(reverse("hello"))
