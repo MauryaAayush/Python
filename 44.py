06e781f359fb2be0d4f89f624565d6d4c8b571be
Admin <maayush235@gmail.com>
2024-04-13 13:37:13 +0530

    Radhey Radhey

--- 44.py
+++ 44.py
@@ -3,12 +3,12 @@
 rows = 5
 for i in range(1, rows + 1):
     for j in range(1, rows - i + 1):
-        print(" ", end="")
+        print(" ", end = " ")
     
     for j in range(1, 2 * i):
         if j == 1 or j == 2 * i - 1 or i == rows:
-            print("*", end=" ")
+            print("*", end = " ")
         else:
-            print(" ", end=" ")
+            print(" ", end = " ")
     
     print()
