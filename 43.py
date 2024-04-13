fe5ef67abb3978f4d15a29ccbec677eef0f2ddd9
Admin <maayush235@gmail.com>
2024-04-13 13:56:33 +0530

    radhey radhey

--- 43.py
+++ 43.py
@@ -1,7 +1,5 @@
 # Function to print full pyramid pattern
 
-A = 65
-
 for i in range(1, 6) :
     A = 65
     for k in range(1 , 6) :
--- 46.py
+++ 46.py
@@ -2,13 +2,14 @@
 
 rows = 5
 for i in range(1, rows + 1):
+    A = 65
     for j in range(1, rows - i + 1):
         print(" ", end = " ")
     
     for j in range(1, 2 * i):
         if j == 1 or j == 2 * i - 1 or i == rows:
-            print(j, end = " ")
+            print(chr(A), end = " ")
         else:
             print(" ", end = " ")
-    
+        A+=1
     print()
