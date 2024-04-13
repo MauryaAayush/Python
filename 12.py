9d91515e1afd2a09c1f461ff1d94f3db06f70dea
Admin <maayush235@gmail.com>
2024-04-13 14:59:54 +0530

    Radhey Radhey

--- 12.py
+++ 12.py
@@ -2,3 +2,10 @@
 # def reverse(string) :
 
 
+name = input("Enter the string : ")
+Rev_name = ""
+
+for char in name :
+    Rev_name = char + Rev_name
+    
+print("Reverse String : ",Rev_name)
--- 13.py
+++ 13.py
@@ -1,1 +1,18 @@
-# program to check a palindrom
+# program to check a palindrome
+
+name  = input("Enter the string here :")
+name = name.lower()
+
+is_palindrome =  True
+
+for i in range(len(name)// 2) :
+    if name[i] !=  name[len(name) - i - 1] :
+        is_palindrome = False
+        break
+    
+
+if is_palindrome:
+    print("This number is a palindrome.")
+    
+else :
+    print("This number not a palindrome")
