28c988e7b880c22972a8e3a1a9bd347098d7a5b2
Admin <maayush235@gmail.com>
2024-04-14 22:37:03 +0530

    Radhey Radhey

--- 34.py
+++ 34.py
@@ -8,7 +8,9 @@ def rectangle_perimeter(length, width):
 
 
 length = float(input("Enter the length of the rectangle: "))
+
 width = float(input("Enter the width of the rectangle: "))
 
 perimeter = rectangle_perimeter(length, width)
+
 print(f"The perimeter of  {length} and width {width} = {perimeter}.")
